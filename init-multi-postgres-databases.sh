12caba756840d34537f9d37e6df929d920d6cb8b
Abdullah Al Muaz <72933395+aurkaxi@users.noreply.github.com>
2026-01-06 22:06:14 +0600

    feat: add explicit user or superuser as db user

--- init-multi-postgres-databases.sh
+++ init-multi-postgres-databases.sh
@@ -6,31 +6,46 @@ set -e
 set -u
 
 function create_databases() {
-    database=$1
-    password=$2
-    echo "Creating user and database '$database' with password '$password'"
+    local db=$1
+    local user=$2
+    local pass=$3
+    
+    echo "Creating Database: '$db' | User: '$user' | Password: '$pass'"
+    
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-      CREATE USER $database with encrypted password '$password';
-      CREATE DATABASE $database;
-      GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+        -- Create user if it doesn't exist (prevents errors if one user owns multiple DBs)
+        DO \$\$
+        BEGIN
+            IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$user') THEN
+                CREATE USER $user WITH ENCRYPTED PASSWORD '$pass';
+            END IF;
+        END
+        \$\$;
+        
+        CREATE DATABASE $db;
+        GRANT ALL PRIVILEGES ON DATABASE $db TO $user;
 EOSQL
 }
 
-
-# POSTGRES_MULTIPLE_DATABASES=db1,db2
-# POSTGRES_MULTIPLE_DATABASES=db1:password,db2
+# POSTGRES_MULTIPLE_DATABASES=db1,db2:usr2,db3:pass3,db4:usr4:pass4
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-  echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-  for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-    user=$(echo $db | awk -F":" '{print $1}')
-    pswd=$(echo $db | awk -F":" '{print $2}')
-    if [[ -z "$pswd" ]]
-    then
-      pswd=$user
-    fi
+    for entry in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+        # Parse the string
+        db_name=$(echo $entry | awk -F":" '{print $1}')
+        db_user=$(echo $entry | awk -F":" '{print $2}')
+        db_pass=$(echo $entry | awk -F":" '{print $3}')
 
-    echo "user is $user and pass is $pswd"
-    create_databases $user $pswd
-  done
-  echo "Multiple databases created!"
+        # Fallback for User
+        if [[ -z "$db_user" ]]; then
+            db_user=$POSTGRES_USER
+        fi
+
+        # Fallback for Password
+        if [[ -z "$db_pass" ]]; then
+            db_pass=$POSTGRES_PASSWORD
+        fi
+
+        create_databases "$db_name" "$db_user" "$db_pass"
+    done
+    echo "Multiple databases created!"
 fi
